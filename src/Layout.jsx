ad565161819bb5da2c371f147ea7766a04414a99
adityajha1023 <adityajha.1023@gmail.com>
2026-01-30 04:08:40 +0530

    INITIAL

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Navigation from './components/Navigation.jsx'
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
+import Navigation from './Components/Navigation'
+import Header from './Components/Header'
+import Footer from './Components/Footer.jsx'
 import { Outlet } from 'react-router-dom'
 
 function Layout() {
